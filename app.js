271f3480ed842c3142706b97b45b0ed6a1a9fe45
Silumesii Maboshe <silumesii@bongohive.co.zm>
2021-07-07 13:00:28 +0200

    Fetch a random user with the Random User API
    https://randomuser.me/

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ const app  = Vue.createApp({
     }
   },
   methods: {
-    getUser() {
-      this.firstName = 'Thandiwe',
-      this.lastName = 'Zulu',
-      this.email = 'thandiwe.zulu@example.com',
-      this.gender = 'female',
-      this.picture = 'https://randomuser.me/api/portraits/women/91.jpg'
+    async getUser() {
+      const response = await fetch('https://randomuser.me/api')
+      const { results } = await response.json()
+
+      //console.log(results)
+
+      this.firstName = results[0].name.first,
+      this.lastName = results[0].name.last,
+      this.email = results[0].email,
+      this.gender = results[0].gender,
+      this.picture = results[0].picture.large
     },
   },
 })
